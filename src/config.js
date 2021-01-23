3df3a3655b973e60718c4cd3fa2b4026b24af177
pavlo kolomoitsev <pavlo.kolomoitsev@nure.ua>
2021-01-23 19:20:01 +0200

    done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    endpoint: "https://sigma-test-backend.herokuapp.com/",
+    endpoint: "https://sigma-test-backend.herokuapp.com",
     refresh: "https://sigma-test-backend.herokuapp.com/user/refresh",
 }
